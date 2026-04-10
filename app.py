689d39dcc2f3f7c9d7043487655e8a90fb0ea879
Egbuchiri Chibuikem Josiah <egbuchirichibuikem@gmail.com>
2026-04-10 23:20:55 +0000

    added imports or helpers files and libary needed for this project

---
+++ app.py
@@ -0,0 +1,27 @@
+import json
+import os
+import sqlite3
+import threading
+import time
+import math
+import re
+from datetime import datetime, timedelta
+from collections import deque
+
+# Data and APIs
+import pandas as pd
+import requests
+import yfinance as yf
+from tradingview_screener import Query
+
+# Web Framework and Security
+from flask import Flask, render_template, jsonify, request, session, flash, redirect
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
+from werkzeug.security import check_password_hash, generate_password_hash
+from dotenv import load_dotenv
+
+# Search and Logic
+from rapidfuzz import fuzz
+
+# Your Custom Logic
+from helpers import get_market_news, get_stock_data
